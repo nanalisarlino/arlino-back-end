deedc22767d323cbda3d906451819fa3da5238a5
nanalisarlino <s22210190@student.unklab.ac.id>
2025-02-10 09:37:51 +0800

    pertemuan7

---
+++ helloworld.js
@@ -0,0 +1,8 @@
+const hello = "Hello World"
+
+const greetings = () => 'Hi, My name is John'
+
+module.exports = {
+    hello,
+    greetings
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const {hello, greetings} = require('./helloworld')
+const moment = require('moment')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.get('/about', (req, res) => res.status(200).json({
+    status : 'success',
+    message : 'about page',
+    data : []
+}))
+
+const hostname = '127.0.0.1';
+const port = 3000;
+app.listen(port, hostname, () => console.log (`Server running at http://${hostname}:${port}`));
