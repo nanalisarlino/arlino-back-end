cd9553de51d4fb84e66e27ad6c4201c177912adb
nanalisarlino <s22210190@student.unklab.ac.id>
2025-02-10 13:52:44 +0800

    perubahan sedikit

--- index.js
+++ index.js
@@ -10,6 +10,26 @@ app.get('/about', (req, res) => res.status(200).json({
     message : 'about page',
     data : []
 }))
+app.post('/contoh', (req, res) => {
+    res.send('request dengan method POST')
+})
+
+app.put('/contoh', (req, res) => {
+    res.send('request dengan method PUT')
+})
+
+app.delete('/contoh', (req, res) => {
+    res.send('request dengan method DELETE')
+})
+
+app.patch('/contoh', (req, res) => {
+    res.send('request dengan method PATCH')
+})
+
+app.all('/universal', (req, res) => res.send(`Artikel ke ${req.method}`))
+//routing dinamis
+//1. Menggunakan params
+app.get('/post/:id', (req, res) => res.send(`Artikel ke ${req.params.id}`))
 
 const hostname = '127.0.0.1';
 const port = 3000;
