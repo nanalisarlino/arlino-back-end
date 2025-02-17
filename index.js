dcd7dd9481082dfbdc664d2fd6f923b2bcf324b0
nanalisarlino <s22210190@student.unklab.ac.id>
2025-02-17 09:58:15 +0800

    middleware

--- index.js
+++ index.js
@@ -1,45 +1,56 @@
-const http = require('http');
-const {hello, greetings} = require('./helloworld')
-const moment = require('moment')
-const express = require('express');
-const morgan = require('morgan');
-const app = express()
+const http = require("http");
+const { hello, greetings } = require("./helloWorld");
+const moment = require("moment");
+const express = require("express");
+const morgan = require("morgan");
+const errorhandler = require("errorhandler");
+const app = express();
 
+//Middleware
 const log = (req, res, next) => {
-    console.log(
-        moment().format("h:mm:ss a" + " " + req.originalUrl + " " + req.ip
-        );
-    next();
-}
+  console.log(
+    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
+  );
+  next();
+};
 
-//routing
-app.get('/', (req, res) => res.send('Hello World'))
-app.get('/about', (req, res) => res.status(200).json({
-    status : 'success',
-    message : 'about page',
-    data : []
-}))
-app.post('/contoh', (req, res) => {
-    res.send('request dengan method POST')
-})
+app.use(morgan("tiny"));
+app.use(errorhandler);
 
-app.put('/contoh', (req, res) => {
-    res.send('request dengan method PUT')
-})
+// Routing
+app.get("/", (req, res) => res.send("Hello World"));
+app.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+app.post("/contoh", (req, res) => res.send("request method POST"));
+app.put("/contoh", (req, res) => res.send("Request method PUT"));
+app.delete("/contoh", (req, res) => res.send("Request method DELETE"));
+app.patch("/contoh", (req, res) => res.send("Request method PATCH"));
 
-app.delete('/contoh', (req, res) => {
-    res.send('request dengan method DELETE')
-})
+app.all("/universal", (req, res) => res.send(`Request method ${req.method}`));
+// Routing dinamis
+// 1. Menggunakan params
+app.get("/post/:id", (req, res) => res.send(`Artikel ke - ${req.params.id}`));
+// 2. Menggunakan Query String
+app.get("/post", (req, res) => {
+  const { page, sort } = req.query;
+  res.send(`Query string= page :${page}, sort : ${sort}`);
+});
 
-app.patch('/contoh', (req, res) => {
-    res.send('request dengan method PATCH')
-})
+//Middleware untuk 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+});
 
-app.all('/universal', (req, res) => res.send(`Artikel ke ${req.method}`))
-//routing dinamis
-//1. Menggunakan params
-app.get('/post/:id', (req, res) => res.send(`Artikel ke ${req.params.id}`))
-
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
-app.listen(port, hostname, () => console.log (`Server running at http://${hostname}:${port}`));
+app.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
