923f91f1e9468f0d3183455aaaddaf2d321f2f22
nanalisarlino <s22210190@student.unklab.ac.id>
2025-04-20 21:00:33 +0800

    Insert, Update, Delete, Get Order User (Join/Aggregate)

---
+++ maincode/index.js
@@ -0,0 +1,50 @@
+//package
+const http = require("http");
+const express = require("express");
+const morgan = require("morgan");
+const moment = require("moment");
+//const errorhandler = require("errorhandler"); //matikan
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+//file lain
+const hello = require("./helloWorld");
+const routers = require("./routers");
+
+//Middleware
+const log = (req, res, next) => {
+  console.log(
+    moment().format("h:mm:ss a") + " " + req.originalUrl + " " + req.ip
+  );
+  next();
+};
+
+app.use(morgan("tiny"));
+//app.use(errorhandler);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join((__dirname, "public"))));
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "PUT"],
+  })
+);
+
+//Routing
+app.use(routers);
+
+//Middleware untuk 404
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "error",
+    message: "resource tidak ditemukan",
+  });
+});
+
+const hostname = "127.0.0.1";
+const port = 3000;
+app.listen(port, hostname, () =>
+  console.log(`Server running at http://${hostname}:${port}`)
+);
---
+++ maincode/routers.js
@@ -0,0 +1,253 @@
+const express = require("express");
+const routers = express.Router();
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const client = require("./mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+routers.get("/users", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const users = await db.collection("users").find().toArray();
+    res.json({
+      status: "success",
+      message: "list users",
+      data: users,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+    });
+  }
+});
+
+routers.get("/users/:id", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const user = await db.collection("users").findOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.status(200).json({
+      status: "success",
+      message: "single user",
+      data: user,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+    });
+  }
+});
+
+// Routing
+const imageFilter = (req, file, cb) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return cb(null, false);
+  }
+  cb(null, true);
+};
+
+const upload = multer({ dest: "public", fileFilter: imageFilter });
+
+//upload file
+routers.post("/upload", upload.single("file"), (req, res) => {
+  const file = req.file;
+  if (file) {
+    const target = path.join(__dirname, "public", file.originalname);
+    fs.renameSync(file.path, target);
+    res.send("file berhasil diupload");
+  } else {
+    res.send("file gagal diupload");
+  }
+});
+
+//buka dan download foto
+routers.get("/bukafoto", (req, res) => {
+  const filename = "logo-uk-white-e1649944158280.png";
+  res.sendFile(__dirname + "/foto/" + filename);
+});
+routers.get("/autodownloadfoto", (req, res) => {
+  const filename = "logo-uk-white-e1649944158280.png";
+  res.download(
+    path.join(__dirname + "/foto/" + filename),
+    "logo-uk-white-e1649944158280.png"
+  );
+});
+
+//masukan username dan password
+routers.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  res.status(200).json({
+    status: "success",
+    message: "Login page",
+    data: {
+      username: username,
+      password: password,
+    },
+  });
+});
+
+//routing biasa menggunakan berbagai request
+routers.get("/", (req, res) => res.send("Hello World"));
+routers.get("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+routers.put("/about", (req, res) =>
+  res.status(200).json({
+    status: "success",
+    message: "About page",
+    data: [],
+  })
+);
+routers.post("/contoh", (req, res) => res.send("request method POST"));
+routers.put("/contoh", (req, res) => res.send("Request method PUT"));
+routers.delete("/contoh", (req, res) => res.send("Request method DELETE"));
+routers.patch("/contoh", (req, res) => res.send("Request method PATCH"));
+
+routers.all("/universal", (req, res) =>
+  res.send(`Request method ${req.method}`)
+);
+
+// Routing dinamis
+// 1. Menggunakan params
+routers.get("/post/:id", (req, res) =>
+  res.send(`Artikel ke - ${req.params.id}`)
+);
+// 2. Menggunakan Query String
+routers.get("/post", (req, res) => {
+  const { page, sort } = req.query;
+  res.send(`Query string= page :${page}, sort : ${sort}`);
+});
+
+// insert user
+routers.post("/users", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const user = await db.collection("users").insertOne(req.body);
+    res.status(200).json({
+      status: "success",
+      message: "Create User",
+      data: user,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+    });
+  }
+});
+// update user
+routers.patch("/users/:id", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const user = await db.collection("users").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      {
+        $set: req.body,
+      }
+    );
+
+    if (user.modifiedCount > 0) {
+      res.status(200).json({
+        status: "success",
+        message: "Update User",
+        data: user,
+      });
+    } else {
+      res.status(404).json({
+        status: "error",
+        message: "User not found or no changes made",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "Failed to update user",
+    });
+  }
+});
+
+// delete user
+routers.delete("/users/:id", async (req, res) => {
+  try {
+    const db = client.db("latihan");
+    const user = await db.collection("users").deleteOne({
+      _id: new ObjectId(req.params.id),
+    });
+    res.status(200).json({
+      status: "success",
+      message: "Delete User",
+      data: user,
+    });
+  } catch (error) {
+    res.json({
+      status: "error",
+    });
+  }
+});
+
+// get order user (join/aggregate)
+routers.get("/order/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({
+        status: "error",
+        message: "Invalid user ID",
+      });
+    }
+
+    const db = client.db("latihan");
+
+    const orders = await db
+      .collection("order")
+      .aggregate([
+        {
+          $match: {
+            UserId: new ObjectId(id),
+          },
+        },
+        {
+          $lookup: {
+            from: "users",
+            localField: "UserId",
+            foreignField: "_id",
+            as: "user_info",
+          },
+        },
+        {
+          $unwind: "$user_info",
+        },
+        {
+          $project: {
+            _id: 1,
+            product: 1,
+            price: 1,
+            "user_info._id": 1,
+            "user_info.name": 1,
+            "user_info.age": 1,
+            "user_info.status": 1,
+          },
+        },
+      ])
+      .toArray();
+
+    res.status(200).json({
+      status: "success",
+      message: "Get Order by User",
+      data: orders,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: "Failed to get order user",
+    });
+  }
+});
+module.exports = routers;
